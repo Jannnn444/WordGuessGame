0d45832743c9a58b548e093e56c6449d9d7b9e29
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-10 11:18:02 +0800

    Initial Commit

---
+++ WordGameRe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGameRe
+//
+//  Created by yucian huang on 2024/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGameRe/WordGameReApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameReApp.swift
+//  WordGameRe
+//
+//  Created by yucian huang on 2024/3/10.
+//
+
+import SwiftUI
+
+@main
+struct WordGameReApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
